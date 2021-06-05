ff3feb78e8c281d675b4a365d3dfc85304421995
ono <insidetail3@gmail.com>
2021-06-05 14:10:16 +0900

    商品一覧機能実装５

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index, :show]
 
   def index
-    @items = Item.all.order(created_at: :desc) 
+    @items = Item.order(created_at: :desc) 
   end
 
   def new
