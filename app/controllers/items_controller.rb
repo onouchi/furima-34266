f5b8163ef6f46340e9e505a381fffd30bdd66582
ono <insidetail3@gmail.com>
2021-06-12 12:43:16 +0900

    商品削除機能実装２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,7 +39,7 @@ class ItemsController < ApplicationController
     if @item.destroy
       redirect_to root_path
     else
-      redirect_to root_path
+      redirect_to item_path
     end
   end
 
